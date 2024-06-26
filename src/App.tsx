7ba0200b30827798d364cb019a8a8b3c63d4d985
David Lafontant <lafontant.david@gmail.com>
2024-06-26 08:10:54 -0400

    Display Navigation and Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
+import { useEffect } from "react";
+import { Navigation, Footer } from "./components/index";
+import 'bootstrap/dist/js/bootstrap.bundle';
+import AddLibrary from './utils/addLibrary';
 import "bootstrap/dist/css/bootstrap.min.css";
 import './App.scss'
 
+
 function App() {
+  useEffect(()=>{AddLibrary('./src/utils/bs-init.js')},[])
 
   return (
     <>
+    <Navigation />
       <div>
         <button className="btn btn-primary">Hello</button>
 
@@ -12,6 +19,7 @@ function App() {
           outer <p className='inner'>inner peace</p>
         </p>
        </div>
+    <Footer />
     </>
   )
 }
