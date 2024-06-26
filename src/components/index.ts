59d1ec39ace6611e08e92405243f7d3cb4c9153d
David Lafontant <lafontant.david@gmail.com>
2024-06-26 09:50:03 -0400

    Add Layout to component barrel pattern file

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,6 @@
-import Navigation from "./navigation/Navigation";
-import Footer from "./footer/Footer";
 
-export {Navigation, Footer};
+import Footer from "./footer/Footer";
+import Layout from "./layout/Layout";
+import Navigation from "./navigation/Navigation";
+
+export { Footer, Layout,   Navigation};
