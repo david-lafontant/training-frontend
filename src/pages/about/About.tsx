2d0bd2ed6e2ed91e313ed54234a9d22a7f2260e0
David Lafontant <lafontant.david@gmail.com>
2024-06-26 21:56:03 -0400

    Add content to the about page

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -2,7 +2,97 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <React.Fragment>
+      <div className="my-5">
+  <div className="p-5 text-center bg-body-tertiary">
+    <div className="container py-5">
+      <h1 className="text-body-emphasis">Centre National d'Education a Distance</h1>
+      <h1 className="text-body-emphasis">C&nbsp;N&nbsp;E&nbsp;D</h1>
+      <p className="col-lg-8 mx-auto lead">
+        This takes the basic jumbotron above and makes its background edge-to-edge with a <code>.container</code> inside to align content. Similar to above, it's been recreated with built-in grid and utility classes.
+      </p>
+    </div>
+  </div>
+</div>
+<div className="container">
+  <div className="row">
+    <div className="col">
+      <h2>Notre vision</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro veritatis quasi ducimus cumque autem odio unde ut sequi. Voluptatibus commodi hic magnam fugiat rerum perspiciatis dolorum ullam ipsa veniam repellendus?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro veritatis quasi ducimus cumque autem odio unde ut sequi. Voluptatibus commodi hic magnam fugiat rerum perspiciatis dolorum ullam ipsa veniam repellendus?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro veritatis quasi ducimus cumque autem odio unde ut sequi. Voluptatibus commodi hic magnam fugiat rerum perspiciatis dolorum ullam ipsa veniam repellendus?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro veritatis quasi ducimus cumque autem odio unde ut sequi. Voluptatibus commodi hic magnam fugiat rerum perspiciatis dolorum ullam ipsa veniam repellendus?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro veritatis quasi ducimus cumque autem odio unde ut sequi. Voluptatibus commodi hic magnam fugiat rerum perspiciatis dolorum ullam ipsa veniam repellendus?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro veritatis quasi ducimus cumque autem odio unde ut sequi. Voluptatibus commodi hic magnam fugiat rerum perspiciatis dolorum ullam ipsa veniam repellendus?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro veritatis quasi ducimus cumque autem odio unde ut sequi. Voluptatibus commodi hic magnam fugiat rerum perspiciatis dolorum ullam ipsa veniam repellendus?
+      </p>
+    </div>
+  </div>
+
+
+
+  <div className="row">
+    <h2>Credo</h2>
+    <div className="col col-md-6">
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro veritatis quasi ducimus cumque autem odio unde ut sequi. Voluptatibus commodi hic magnam fugiat rerum perspiciatis dolorum ullam ipsa veniam repellendus?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro veritatis quasi ducimus cumque autem odio unde ut sequi. Voluptatibus commodi hic magnam fugiat rerum perspiciatis dolorum ullam ipsa veniam repellendus?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro veritatis quasi ducimus cumque autem odio unde ut sequi. Voluptatibus commodi hic magnam fugiat rerum perspiciatis dolorum ullam ipsa veniam repellendus?
+      </p>
+    </div>
+    <div className="col col-md-6">
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro veritatis quasi ducimus cumque autem odio unde ut sequi. Voluptatibus commodi hic magnam fugiat rerum perspiciatis dolorum ullam ipsa veniam repellendus?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro veritatis quasi ducimus cumque autem odio unde ut sequi. Voluptatibus commodi hic magnam fugiat rerum perspiciatis dolorum ullam ipsa veniam repellendus?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro veritatis quasi ducimus cumque autem odio unde ut sequi. Voluptatibus commodi hic magnam fugiat rerum perspiciatis dolorum ullam ipsa veniam repellendus?
+      </p>
+    </div>
+    <div className="col col-md-6">
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro veritatis quasi ducimus cumque autem odio unde ut sequi. Voluptatibus commodi hic magnam fugiat rerum perspiciatis dolorum ullam ipsa veniam repellendus?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro veritatis quasi ducimus cumque autem odio unde ut sequi. Voluptatibus commodi hic magnam fugiat rerum perspiciatis dolorum ullam ipsa veniam repellendus?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro veritatis quasi ducimus cumque autem odio unde ut sequi. Voluptatibus commodi hic magnam fugiat rerum perspiciatis dolorum ullam ipsa veniam repellendus?
+      </p>
+    </div>
+    <div className="col col-md-6">
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro veritatis quasi ducimus cumque autem odio unde ut sequi. Voluptatibus commodi hic magnam fugiat rerum perspiciatis dolorum ullam ipsa veniam repellendus?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro veritatis quasi ducimus cumque autem odio unde ut sequi. Voluptatibus commodi hic magnam fugiat rerum perspiciatis dolorum ullam ipsa veniam repellendus?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro veritatis quasi ducimus cumque autem odio unde ut sequi. Voluptatibus commodi hic magnam fugiat rerum perspiciatis dolorum ullam ipsa veniam repellendus?
+      </p>
+    </div>
+  </div>
+</div>
+    </React.Fragment>
   )
 }
 
